08d978d0164329dde9b5eee53470e04b12845492
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-08 14:15:12 +0530

    Basic Layout Component

---
+++ components/layout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./layout.module.css";
+
+const Layout = ({ children }) => {
+  return <div className={styles.container}>{children}</div>;
+};
+
+export default Layout;
