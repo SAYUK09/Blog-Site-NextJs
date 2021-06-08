4772adc12e1fc287137340cd594ff7f9bfcbfde8
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-08 14:48:44 +0530

    Updated Layout and Basic Template Ssetup

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
-import React from "react";
-import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <h1>Welcome to nextJS</h1>
-
-      <h2 className="title">
-        {`Read `}
-        <Link href="/posts/first-post">
-          <a>this page! {""}</a>
-        </Link>
-      </h2>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi there 👋, Hey, I am Sayuri Hi, I am Sayuri a young 18 years old
+          girl on a journey to enchance as a Fullstack Web Developer. I code in
+          HTML, CSS, Javascript and React. <br />I love communicating, making
+          new connections and discussing about ideas, projects and startup.{" "}
+          <br />
+          Also I document my journey on some social networking sites
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
