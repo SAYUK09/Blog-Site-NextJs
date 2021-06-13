c6f6e1fefa201a32ee25671b3393e69343157012
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-13 23:21:52 +0530

    checkbox slider

--- components/toggleButton.jsx
+++ components/toggleButton.jsx
@@ -1,18 +1,30 @@
-import React from "react";
 import { useTheme } from "../themeContext";
+import styles from "../styles/toggleButton.module.css";
 
 const ToggleButton = () => {
   const { darkTheme, setDarkTheme } = useTheme();
 
   return (
     <div>
-      <button
+      {/* <button
         onClick={() => {
           setDarkTheme(!darkTheme);
         }}
       >
         DARK
-      </button>
+      </button> */}
+      <label className={styles.switch}>
+        <i className={styles.fas}></i>
+        <div>
+          <input
+            onClick={() => {
+              setDarkTheme(!darkTheme);
+            }}
+            type="checkbox"
+          />
+          <span className={`${styles.slider} ${styles.slider}`}></span>
+        </div>
+      </label>
     </div>
   );
 };
